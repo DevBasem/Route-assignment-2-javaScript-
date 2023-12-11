65eb2b79734c533020344d6cd7fe0857eda2e1c4
DevBasem <engr.basemahmed@gmail.com>
2023-12-11 15:45:19 +0200

    Route assignment 2 (javaScript)

---
+++ js/main.js
@@ -0,0 +1,99 @@
+let quotes = [
+  {quote: `Be yourself; everyone else is already taken.`, author: 'Oscar Wilde'},
+  {quote: `I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.`, author: 'Marilyn Monroe'},
+  {quote: `Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.`, author: 'Albert Einstein'},
+  {quote: `So many books, so little time.`, author: 'Frank Zappa'},
+  {quote: `A room without books is like a body without a soul.`, author: 'Marcus Tullius Cicero'},
+  {quote: `Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.`, author: 'Bernard M. Baruch'},
+  {quote: `You've gotta dance like there's nobody watching, love like you'll never be hurt, sing like there's nobody listening, and live like it's heaven on earth.`, author: 'William W. Purkey'},
+  {quote: `You know you're in love when you can't fall asleep because reality is finally better than your dreams.`, author: 'Dr. Seuss'},
+  {quote: `The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle.`, author: 'Steve Jobs'},
+  {quote: `Success is not final, failure is not fatal: It is the courage to continue that counts.`, author: 'Winston Churchill'},
+  {quote: `The greatest glory in living lies not in never falling, but in rising every time we fall.`, author: 'Nelson Mandela'},
+  {quote: `The purpose of our lives is to be happy.`, author: 'Dalai Lama'},
+  {quote: `Life is what happens when you're busy making other plans.`, author: 'John Lennon'},
+  {quote: `Get busy living or get busy dying.`, author: 'Stephen King'},
+  {quote: `The only limit to our realization of tomorrow will be our doubts of today.`, author: 'Franklin D. Roosevelt'},
+  {quote: `Life is really simple, but we insist on making it complicated.`, author: 'Confucius'},
+  {quote: `In three words I can sum up everything I've learned about life: it goes on.`, author: 'Robert Frost'},
+  {quote: `Life is either a daring adventure or nothing at all.`, author: 'Helen Keller'},
+  {quote: `To live is the rarest thing in the world. Most people exist, that is all.`, author: 'Oscar Wilde'},
+  {quote: `Life is what happens when you're busy making other plans.`, author: 'John Lennon'},
+  {quote: `The purpose of our lives is to be happy.`, author: 'Dalai Lama'},
+  {quote: `Life is really simple, but we insist on making it complicated.`, author: 'Confucius'},
+  {quote: `In three words I can sum up everything I've learned about life: it goes on.`, author: 'Robert Frost'},
+  {quote: `Life is either a daring adventure or nothing at all.`, author: 'Helen Keller'},
+  {quote: `To live is the rarest thing in the world. Most people exist, that is all.`, author: 'Oscar Wilde'},
+  {quote: `The purpose of our lives is to be happy.`, author: 'Dalai Lama'},
+  {quote: `Life is really simple, but we insist on making it complicated.`, author: 'Confucius'},
+  {quote: `In three words I can sum up everything I've learned about life: it goes on.`, author: 'Robert Frost'},
+  {quote: `Life is either a daring adventure or nothing at all.`, author: 'Helen Keller'},
+  {quote: `To live is the rarest thing in the world. Most people exist, that is all.`, author: 'Oscar Wilde'},
+  {quote: `Life is what happens when you're busy making other plans.`, author: 'John Lennon'},
+  {quote: `The purpose of our lives is to be happy.`, author: 'Dalai Lama'},
+  {quote: `Life is really simple, but we insist on making it complicated.`, author: 'Confucius'},
+  {quote: `In three words I can sum up everything I've learned about life: it goes on.`, author: 'Robert Frost'},
+  {quote: `Life is either a daring adventure or nothing at all.`, author: 'Helen Keller'},
+  {quote: `To live is the rarest thing in the world. Most people exist, that is all.`, author: 'Oscar Wilde'},
+  {quote: `The purpose of our lives is to be happy.`, author: 'Dalai Lama'},
+  {quote: `Life is really simple, but we insist on making it complicated.`, author: 'Confucius'},
+  {quote: `In three words I can sum up everything I've learned about life: it goes on.`, author: 'Robert Frost'},
+  {quote: `Life is either a daring adventure or nothing at all.`, author: 'Helen Keller'},
+  {quote: `To live is the rarest thing in the world. Most people exist, that is all.`, author: 'Oscar Wilde'},
+  {quote: `The purpose of our lives is to be happy.`, author: 'Dalai Lama'},
+  {quote: `Life is really simple, but we insist on making it complicated.`, author: 'Confucius'},
+  {quote: `In three words I can sum up everything I've learned about life: it goes on.`, author: 'Robert Frost'},
+  {quote: `Life is either a daring adventure or nothing at all.`, author: 'Helen Keller'},
+  {quote: `To live is the rarest thing in the world. Most people exist, that is all.`, author: 'Oscar Wilde'},
+  {quote: `The purpose of our lives is to be happy.`, author: 'Dalai Lama'},
+  {quote: `Life is really simple, but we insist on making it complicated.`, author: 'Confucius'},
+  {quote: `In three words I can sum up everything I've learned about life: it goes on.`, author: 'Robert Frost'},
+  {quote: `Life is either a daring adventure or nothing at all.`, author: 'Helen Keller'},
+  {quote: `To live is the rarest thing in the world. Most people exist, that is all.`, author: 'Oscar Wilde'},
+  {quote: `The purpose of our lives is to be happy.`, author: 'Dalai Lama'},
+  {quote: `Life is really simple, but we insist on making it complicated.`, author: 'Confucius'},
+  {quote: `In three words I can sum up everything I've learned about life: it goes on.`, author: 'Robert Frost'},
+  {quote: `Life is either a daring adventure or nothing at all.`, author: 'Helen Keller'},
+  {quote: `To live is the rarest thing in the world. Most people exist, that is all.`, author: 'Oscar Wilde'},
+  {quote: `The purpose of our lives is to be happy.`, author: 'Dalai Lama'},
+  {quote: `Life is really simple, but we insist on making it complicated.`, author: 'Confucius'},
+  {quote: `In three words I can sum up everything I've learned about life: it goes on.`, author: 'Robert Frost'},
+  {quote: `Life is either a daring adventure or nothing at all.`, author: 'Helen Keller'},
+  {quote: `To live is the rarest thing in the world. Most people exist, that is all.`, author: 'Oscar Wilde'},
+  {quote: `The purpose of our lives is to be happy.`, author: 'Dalai Lama'},
+  {quote: `Life is really simple, but we insist on making it complicated.`, author: 'Confucius'},
+  {quote: `In three words I can sum up everything I've learned about life: it goes on.`, author: 'Robert Frost'},
+  {quote: `Life is either a daring adventure or nothing at all.`, author: 'Helen Keller'},
+  {quote: `To live is the rarest thing in the world. Most people exist, that is all.`, author: 'Oscar Wilde'},
+  {quote: `The purpose of our lives is to be happy.`, author: 'Dalai Lama'},
+  {quote: `Life is really simple, but we insist on making it complicated.`, author: 'Confucius'},
+  {quote: `In three words I can sum up everything I've learned about life: it goes on.`, author: 'Robert Frost'},
+  {quote: `Life is either a daring adventure or nothing at all.`, author: 'Helen Keller'},
+  {quote: `To live is the rarest thing in the world. Most people exist, that is all.`, author: 'Oscar Wilde'},
+];
+
+//Global Variable To Save The State Of The Last Generated Index.
+let lastIndexNumber = null;
+//A Function To Get Random Quote From The Quotes Array.
+const getQuote = () => 
+{
+  let currentIndexNumber;
+
+  /*Do - While Loop To At Least Get The Random Number For The Current Index Once 
+  Before Checking If It Equals The Last Index Or Not.*/
+  do
+  {
+    currentIndexNumber = Math.floor(Math.random() * (quotes.length));
+  }
+  while (currentIndexNumber === lastIndexNumber);
+
+  //Saving The State Of The Current Number In The lastIndexNumber Global Variable.
+  lastIndexNumber = currentIndexNumber;
+  console.log(currentIndexNumber);
+  //Assigning The Current Quote From The Array Of Objects Into The HTML.
+  output1.innerHTML = quotes[currentIndexNumber].quote;
+  //Assigning The Current Author Of The Current Quote From The Array Of Objects Into The HTML.
+  output2.innerHTML = 'By: '+quotes[currentIndexNumber].author;
+
+  return currentIndexNumber;
+}
